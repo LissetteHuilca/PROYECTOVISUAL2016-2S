3b7db551a9db55eca582b13614b799244720abe5
LissetteHuilca <lissette.huilca@gmail.com>
2016-11-30 21:58:50 -0500

    Holaa

--- ProyectoVotaciones2017/ProyectoVotaciones2017/Module1.vb
+++ ProyectoVotaciones2017/ProyectoVotaciones2017/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hola le mando un saludo diciendole que la quiero mucho jejeejej ")
+        Console.WriteLine("Hola le mando un saludo diciendole que la quiero mucho jejeejej     ")
     End Sub
 
 End Module
